7c6980c497ac2330a34f4ec0a03a8bbf9c84996b
nazSenuh <nazarii.seniukh@gmail.com>
2024-01-11 18:38:48 +0200

    balancer

--- server/src/api/api.router.ts
+++ server/src/api/api.router.ts
@@ -4,12 +4,22 @@ import authRouter from "../auth/router/auth.router";
 import slipRouter from "../slips/router/slip.router";
 import reservationRouter from "../reservation/router/reservation.router";
 import seasonRouter from "../season/router/season.router";
+import * as os from 'node:os';
 
 export class AppRouter {
-  constructor(private app: Application) {}
+  constructor(private app: Application) { }
 
   init() {
-    this.app.get("/", (_: Request, res: Response) => res.send("bootstrap"));
+    this.app.get("/", (_: Request, res: Response) => {
+      const networkInterfaces = os.networkInterfaces()
+
+      const ipAddress = Object.keys(networkInterfaces)
+        .map(interfaceName => networkInterfaces[interfaceName]!.find(item => item.family === 'IPv4' && !item.internal))
+        .filter(Boolean)[0]?.address;
+            
+      res.send(`boostrap ${ipAddress}`)
+    });
+
     this.app.use("/api/auth", authRouter);
     this.app.use("/api/users", userRouter);
     this.app.use("/api/slips", slipRouter);
